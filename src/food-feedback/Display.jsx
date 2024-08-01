42066b22a43c7a1641e2948ede5d243595f7de0b
Mohan Basnet <basnet.mohan2030@gmail.com>
2024-08-01 18:58:26 +0545

    Refactoring code

--- src/food-feedback/Display.jsx
+++ src/food-feedback/Display.jsx
@@ -1,17 +1,11 @@
 const Display = (props) => {
-    let good = props.values.good
-    let neutral = props.values.neutral
-    let bad = props.values.bad
-
-    const avg = () => {
-        return (good+neutral+bad)/3
-    }
+    const {good, neutral, bad} = props.values
 
     return (
         <div>
-            <p>Good: {props.values.good}</p>
-            <p>Neutral: {props.values.neutral}</p>
-            <p>Bad: {props.values.bad}</p>
+            <p>Good: {good}</p>
+            <p>Neutral: {neutral}</p>
+            <p>Bad: {bad}</p>
             <p>Avg: {(Math.round(good+neutral+bad)/3).toFixed(2)} </p>
             <p>Positive: {(Math.round(good)/(good+neutral+bad)*100).toFixed(2)}% </p>
         </div>
